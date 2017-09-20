5fc43022ef20d919b2478c1784c4768db57071fd
Ken Vilar <kenvilar@gmail.com>
2017-09-21 05:38:59 +0800

    Bootstrap __construct

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -1,1 +1,23 @@
 <?php
+
+class Bootstrap {
+	private $controller;
+	private $action;
+	private $request;
+	
+	public function __construct( $request ) {
+		$this->request = $request;
+		
+		if ( $this->request['controller'] == "" ) {
+			//TODO
+		} else {
+			//TODO
+		}
+		
+		if ( $this->request['action'] == "" ) {
+			//TODO
+		} else {
+			//TODO
+		}
+	}
+}
