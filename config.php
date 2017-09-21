7a3df17439881d7f6ad341aa34cbe78d2e1a8e90
Ken Vilar <kenvilar@gmail.com>
2017-09-21 22:15:40 +0800

    Define database parameters

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Define DB Constants, this is only a sample
+define( 'DB_NAME', 'sample_db_name' );
+define( 'DB_HOST', 'localhost' );
+define( 'DB_USER', 'sample_user' );
+define( 'DB_PASS', 'sample_password' );
+
+/* Define URL */
+// For Development
+define( 'ROOT_URL', '/bull-stock-readers/' );
+define( 'ROOT_PATH', 'http://localhost/bull-stock-readers/' );
+
+//For Production
+define( '', '' );
+define( '', '' );
