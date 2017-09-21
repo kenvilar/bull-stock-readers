06f56010c742fd9ee87778e3013c994ef37bab8c
Ken Vilar <kenvilar@gmail.com>
2017-09-21 22:01:03 +0800

    Instantiate classes

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+session_start();
+
+// TODO create config
+
+// Classes
+require 'classes/Bootstrap.php';
+require 'classes/Controller.php';
+
+// Controllers
+require 'controllers/home.php';
+
+$bootstrap = new Bootstrap( $_GET );
+// TODO instantiate controller class
