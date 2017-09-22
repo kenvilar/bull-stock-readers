cf3da41c17087195944619e9b41ac49c9d8daf96
Ken Vilar <kenvilar@gmail.com>
2017-09-22 23:02:59 +0800

    Create main view

---
+++ views/main.php
@@ -0,0 +1,14 @@
+<?php
+/** Main View **/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bull Stock Readers</title>
+</head>
+<body>
+	<!-- TODO: Apply Bootstrap -->
+</body>
+</html>
