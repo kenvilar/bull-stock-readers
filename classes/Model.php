65eb66ca4afd724af6ae8df186841556f501dead
Ken Vilar <kenvilar@gmail.com>
2017-09-22 22:40:50 +0800

    Prepares PDO statement for execution

--- classes/Model.php
+++ classes/Model.php
@@ -8,4 +8,48 @@ class Model {
 		$dsn       = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST;
 		$this->dbh = new PDO( $dsn, DB_USER, DB_PASS );
 	}
+	
+	public function query( $query ) {
+		$this->stmt = $this->dbh->prepare( $query );
+	}
+	
+	public function bind( $param, $value, $type ) {
+		if ( is_null( $type ) ) {
+			switch ( true ) {
+				case is_int( $type ) :
+					$type = PDO::PARAM_INT;
+					break;
+				case is_bool( $type ) :
+					$type = PDO::PARAM_BOOL;
+					break;
+				case is_null( $type ) :
+					$type = PDO::PARAM_NULL;
+					break;
+				default :
+					$type = PDO::PARAM_STR;
+					break;
+			}
+		}
+		$this->stmt->bindValue( $param, $value, $type );
+	}
+	
+	public function execute() {
+		$this->stmt->execute();
+	}
+	
+	public function resultSet() {
+		$this->execute();
+		
+		return $this->stmt->fetchAll( PDO::FETCH_ASSOC );
+	}
+	
+	public function single() {
+		$this->execute();
+		
+		return $this->stmt->fetch( PDO::FETCH_ASSOC );
+	}
+	
+	public function lastInsertId() {
+		return $this->dbh->lastInsertId();
+	}
 }
