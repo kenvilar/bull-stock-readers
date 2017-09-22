927af344af9d38ecb7e66a6bd527def2f12f663f
Ken Vilar <kenvilar@gmail.com>
2017-09-22 22:30:16 +0800

    Controller _construct()

--- classes/Controller.php
+++ classes/Controller.php
@@ -1,5 +1,19 @@
 <?php
 
 class Controller {
-
+	protected $request;
+	protected $action;
+	
+	public function __construct( $action, $request ) {
+		$this->action  = $action;
+		$this->request = $request;
+	}
+	
+	public function executeAction() {
+		return $this->{$this->action}();
+	}
+	
+	protected function returnView() {
+		//TODO
+	}
 }
