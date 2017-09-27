6bc1914c73554513362681f2712ff475d954479b
Ken Vilar <kenvilar@gmail.com>
2017-09-27 23:36:38 +0800

    Load config file, message alerts and model classes

--- index.php
+++ index.php
@@ -2,14 +2,23 @@
 
 session_start();
 
-// TODO create config
+// Config
+require 'config.php';
 
 // Classes
 require 'classes/Bootstrap.php';
+require 'classes/MessageAlerts.php';
 require 'classes/Controller.php';
+require 'classes/Model.php';
 
 // Controllers
 require 'controllers/home.php';
 
-$bootstrap = new Bootstrap( $_GET );
-// TODO instantiate controller class
+// Models
+require 'models/home.php';
+
+$bootstrap  = new Bootstrap( $_GET );
+$controller = $bootstrap->createController();
+if ( $controller ) {
+	$controller->executeAction();
+}
