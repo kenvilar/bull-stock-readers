bf3f48c26289ce56d7b9810351756c67a9669d01
Ken Vilar <kenvilar@gmail.com>
2017-09-29 00:52:16 +0800

    Set login page for unregistered users only

--- views/user/login.php
+++ views/user/login.php
@@ -1,28 +1,36 @@
 <?php
 /** Login Page **/
-?>
 
-<div class="panel panel-default">
-	<div class="panel-heading">
-		<h3 class="panel-title">User Login</h3>
-	</div>
-	<div class="panel-body">
-		<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
-			<div class="form-group">
-				<label for="email">Email</label>
-				<input type="text" name="email" id="email" class="form-control"/>
-			</div>
-			<div class="form-group">
-				<label for="password">Password</label>
-				<input type="password" name="password" id="password" class="form-control"/>
-			</div>
-			<input class="btn btn-primary btn-lg" type="submit" name="submit" value="Log in">
-		</form>
-	</div>
-</div>
+if ( ! isset( $_SESSION['is_logged_in'] ) ) : ?>
 
-<div class="container">
-	<div class="row">
-		<p>If you don't have an account you can <a href="<?php echo ROOT_URL; ?>user/register">sign up</a> for one now.</p>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3 class="panel-title">User Login</h3>
+		</div>
+		<div class="panel-body">
+			<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="text" name="email" id="email" class="form-control"/>
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" class="form-control"/>
+				</div>
+				<input class="btn btn-primary btn-lg" type="submit" name="submit" value="Log in">
+			</form>
+		</div>
 	</div>
-</div>
+
+	<div class="container">
+		<div class="row">
+			<p>If you don't have an account you can <a href="<?php echo ROOT_URL; ?>user/register">sign up</a> for one
+				now.</p>
+		</div>
+	</div>
+	
+	<?php
+
+else :
+	header( 'Location: ' . ROOT_URL );
+endif;
