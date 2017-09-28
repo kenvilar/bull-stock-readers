fd66954de8fb9a346c481b3de9344206ef10d783
Ken Vilar <kenvilar@gmail.com>
2017-09-28 17:31:37 +0800

    Create User Model

---
+++ models/user.php
@@ -0,0 +1,46 @@
+<?php
+
+class UserModel extends Model {
+	public function register() {
+		$post = filter_input_array( INPUT_POST, FILTER_SANITIZE_STRING );
+		
+		if ( $post['submit'] ) {
+			if ( $post['name'] == "" || $post['email'] == "" || $post['password'] == "" ) {
+				MessageAlerts::setMsg( 'Please fill in all the fields!', 'error' );
+			}
+			$this->query( 'INSERT INTO users ( name, email, password ) VALUES ( :name, :email, :password )' );
+			$this->bind( ':name', $post['name'] );
+			$this->bind( ':email', $post['email'] );
+			$this->bind( ':password', $post['password'] );
+			$this->execute();
+		}
+		
+		return;
+	}
+	
+	public function login() {
+		$post = filter_input_array( '', '' );
+		
+		if ( $post['submit'] ) {
+			$this->query( 'SELECT * FROM users WHERE email = :email AND password = :password' );
+			$this->bind( ':email', $post['email'] );
+			$this->bind( ':password', $post['password'] );
+			
+			$user_exist = $this->single();
+			
+			if ( $user_exist ) {
+				$_SESSION['is_logged_in'] = true;
+				$_SESSION['user_data']    = array(
+					'id'    => $user_exist['id'],
+					'name'  => $user_exist['name'],
+					'email' => $user_exist['email'],
+				);
+				header( 'Location: ' . ROOT_URL . 'stocks' );
+			} else {
+				MessageAlerts::setMsg( 'Incorrect email or password', 'error' );
+			}
+		}
+		
+		return;
+	}
+}
