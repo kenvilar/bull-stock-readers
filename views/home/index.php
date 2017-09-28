1fe6c5228d1bc5659a266bfe223ffee1762c02ce
Ken Vilar <kenvilar@gmail.com>
2017-09-28 18:18:11 +0800

    Develop the homepage template

---
+++ views/home/index.php
@@ -0,0 +1,8 @@
+<?php
+/** Home Page **/
+?>
+
+<div class="text-center">
+	<h1>Welcome To Bull Stock Reader</h1>
+	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH; ?>stocks">List of Stocks</a>
+</div>
