815c53d398b6f1de6e36343be4ef800b03499605
Ken Vilar <kenvilar@gmail.com>
2017-09-28 20:17:33 +0800

    Hide list of stocks button for registered user

--- views/home/index.php
+++ views/home/index.php
@@ -4,5 +4,7 @@
 
 <div class="text-center">
 	<h1>Welcome To Bull Stock Reader</h1>
-	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH; ?>stocks">List of Stocks</a>
+	<?php if ( isset( $_SESSION['is_logged_in'] ) ) : ?>
+		<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH; ?>stocks">List of Stocks</a>
+	<?php endif; ?>
 </div>
