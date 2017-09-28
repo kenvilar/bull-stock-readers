c9d298405287bc0ae874640567530d014484cbc7
Ken Vilar <kenvilar@gmail.com>
2017-09-28 17:15:12 +0800

    Login page

---
+++ views/user/login.php
@@ -0,0 +1,22 @@
+<?php
+/** Login Page **/
+?>
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">User Login</h3>
+	</div>
+	<div class="panel-body">
+		<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+			<div class="form-group">
+				<label for="email">Email</label>
+				<input type="text" name="email" id="email" class="form-control"/>
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input type="password" name="password" id="password" class="form-control"/>
+			</div>
+			<input class="btn btn-primary" name="submit" type="submit" value="Submit"/>
+		</form>
+	</div>
+</div>
