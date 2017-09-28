afa0ad0e697783768c54fa4eff2225119cdac6a8
Ken Vilar <kenvilar@gmail.com>
2017-09-28 23:22:52 +0800

    Modify sample database constants

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 // Define DB Constants, this is only a sample
-define( 'DB_NAME', 'sample_db_name' );
+define( 'DB_NAME', 'sample_bsr' );
 define( 'DB_HOST', 'localhost' );
-define( 'DB_USER', 'sample_user' );
-define( 'DB_PASS', 'sample_password' );
+define( 'DB_USER', 'sample_bsr' );
+define( 'DB_PASS', 'W1hqX0fGx3fW1sjL' );
 
 /* Define URL */
 // For Development
