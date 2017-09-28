bf6c8ad5f432ddb36a57a61ab82838d5ca631f27
Ken Vilar <kenvilar@gmail.com>
2017-09-28 18:15:57 +0800

    Assign list of stocks with a variable

--- models/stocks.php
+++ models/stocks.php
@@ -3,7 +3,8 @@
 class StocksModel extends Model {
 	public function Index() {
 		$this->query( 'SELECT * FROM stocks ORDER BY title DESC' );
+		$rows = $this->resultSet();
 		
-		return $this->resultSet();
+		return $rows;
 	}
 }
