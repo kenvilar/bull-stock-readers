f1dfaeb785b9bcb97170c8b13199cdc5a16823a3
Ken Vilar <kenvilar@gmail.com>
2017-09-28 18:24:12 +0800

    Change register function visibility on usermodel

--- models/user.php
+++ models/user.php
@@ -1,7 +1,7 @@
 <?php
 
 class UserModel extends Model {
-	public function register() {
+	protected function register() {
 		$post = filter_input_array( INPUT_POST, FILTER_SANITIZE_STRING );
 		
 		$password = md5( $post['password'] );
