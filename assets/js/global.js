515016721e11b945be807ed980b450ee6cb1a86c
Ken Vilar <kenvilar@gmail.com>
2017-09-29 13:01:04 +0800

    Initialize function for typing animation

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,7 +1,16 @@
 (function( $ ) {
 
 	$( document ).ready( function() {
-		// TODO Text typing animation
+		// Text typing animation
+		var TxtType = function( el, toType, period ) {
+			this.toType = toType;
+			this.el = el;
+			this.loop = 0;
+			this.period = parseInt( period, 10 ) || 2000;
+			this.txt = '';
+			this.tick();
+			this.toDelete = false;
+		};
 	} );
 
 })( jQuery );
