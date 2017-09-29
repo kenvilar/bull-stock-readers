d6fb480b0e1e83c735ab7f523adb68b541b67807
Ken Vilar <kenvilar@gmail.com>
2017-09-30 02:39:44 +0800

    Modify home index page

--- views/home/index.php
+++ views/home/index.php
@@ -3,7 +3,8 @@
 ?>
 
 <div class="text-center">
-	<h1>Welcome To Bull Stock Readers</h1>
+	<h1 class="bullstockreader-type-animation" data-brs-typing-interval="500"
+	    data-brs-typing='[ "Welcome To Bull Stock Readers", "Have Fun!" ]'></h1>
 	<?php if ( isset( $_SESSION['is_logged_in'] ) ) : ?>
 		<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH; ?>stocks">List of Stocks</a>
 	<?php else : ?>
@@ -11,9 +12,7 @@
 			<div class="row">
 				<div class="col-md-6 jumbotron">
 					<h2>What is Bull Stock Readers?</h2>
-					<p>A <span class="bullstockreader-type-animation" bull-stock-reader-text-animation-interval="2000"
-					           bull-stock-reader-text-animation='[  "simple", "pure", "fun" ]'></span> stock market
-						news, analysis and blog sharing web app for traders and investors.</p>
+					<p>A simple stock market news, analysis and blog sharing web app for traders and investors.</p>
 				</div>
 
 				<div class="col-md-6 text-center">
